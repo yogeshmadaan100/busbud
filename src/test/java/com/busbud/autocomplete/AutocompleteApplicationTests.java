cf8decc8e9eaeadc2536caa8e9542183036e2ccb
Yogesh <yogesh.madaan@grabtaxi.com>
2019-11-22 18:33:55 +0800

    Init Project

---
+++ src/test/java/com/busbud/autocomplete/AutocompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busbud.autocomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
